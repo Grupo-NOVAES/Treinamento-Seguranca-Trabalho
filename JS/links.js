514c122889d12bcf3528725a7157549a6abd4b6f
Brxnin-FX <brunosantiago7002@gmail.com>
2024-08-28 10:54:30 -0300

    create links.js

---
+++ JS/links.js
@@ -0,0 +1,7 @@
+const  LoginPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/login.html";
+const   VideoPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/videos.html";
+const   FormsPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/indexForms.html";
+const  thankYouPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/thanks.html";
+const   HomePage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/index.html";
+
+export default { VideoPage, HomePage, FormsPage,  LoginPage, thankYouPage}
