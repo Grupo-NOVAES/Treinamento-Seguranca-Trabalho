0abc178b207eca55814ae7b21f2c675b7b1b6c88
Brxnin-FX <brunosantiago7002@gmail.com>
2024-08-28 10:46:42 -0300

    put the links.js in gitignore

--- JS/links.js
+++
@@ -1,7 +0,0 @@
-const  LoginPage = "../HTML/login.html";
-const   VideoPage = "../HTML/videos.html";
-const   FormsPage = "../HTML/indexForms.html";
-const  thankYouPage = "../HTML/thanks.html";
-const   HomePage = "../index.html";
-
-export default { VideoPage, HomePage, FormsPage,  LoginPage, thankYouPage}
