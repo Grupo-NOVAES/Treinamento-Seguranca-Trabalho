cf67d5ba375adf9ebfa6b2fa4fffffaa99f5b135
Brxnin-FX <brunosantiago7002@gmail.com>
2024-09-25 08:17:16 -0300

    back commit

--- JS/sidebar.js
+++
@@ -1,7 +0,0 @@
-let bodyElement = document.querySelector('body');
-
-const barraLateral = document.querySelector(".BarraLateral");
-
-barraLateral.addEventListener('click',() => {
-  barraLateral.classList.toggle("close");
-})
