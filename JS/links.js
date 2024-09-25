d5b4aab27d180b2d6fe3809bb3dedc53edbed3aa
Brxnin-FX <brunosantiago7002@gmail.com>
2024-09-25 08:46:43 -0300

    repair links

--- JS/links.js
+++ JS/links.js
@@ -1,7 +1,7 @@
-const  LoginPage = "../HTML/login.html";
-const   VideoPage = "../HTML/videos.html";
-const   FormsPage = "../HTML/indexForms.html";
-const  thankYouPage = "../HTML/thanks.html";
-const   HomePage = "../index.html";
+const  LoginPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/login.html";
+const   VideoPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/videos.html";
+const   FormsPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/indexForms.html";
+const  thankYouPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/thanks.html";
+const   HomePage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/index.html";
 
 export default { VideoPage, HomePage, FormsPage,  LoginPage, thankYouPage}
