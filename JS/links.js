5963761b2c000271a22510968fab9b87adc1ed7b
Bruno <124815320+Bruno1776@users.noreply.github.com>
2025-06-24 18:41:24 -0300

    return videos

--- JS/links.js
+++ JS/links.js
@@ -1,5 +1,5 @@
 const  LoginPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/login.html";
-const   VideoPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/indexForms.html";
+const   VideoPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/videos.html";
 const   FormsPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/indexForms.html";
 const  thankYouPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/thanks.html";
 const   HomePage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/index.html";
