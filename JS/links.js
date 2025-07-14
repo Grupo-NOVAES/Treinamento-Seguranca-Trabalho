ca6a24c580c115f494011f8a1ff1fc1bf7ad560b
GrupoNOVAES <inovacao@novaes.eng.br>
2025-07-14 16:22:44 -0300

    teste links

--- JS/links.js
+++ JS/links.js
@@ -1,9 +1,3 @@
-//const  LoginPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/HTML/login.html";
-//const   VideoPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/HTML/videos.html";
-//const   FormsPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/HTML/indexForms.html";
-//const  thankYouPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/HTML/thanks.html";
-//const   HomePage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/index.html";
-
 const  LoginPage = "/HTML/login.html";
 const   VideoPage = "/HTML/videos.html";
 const   FormsPage = "/HTML/indexForms.html";
