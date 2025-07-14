5652bb1b980b2e6d04fc3d33137dc7d8d13d165a
GrupoNOVAES <inovacao@novaes.eng.br>
2025-07-14 16:17:22 -0300

    fix link

--- JS/links.js
+++ JS/links.js
@@ -1,8 +1,8 @@
-const  LoginPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/login.html";
-const   VideoPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/videos.html";
-const   FormsPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/indexForms.html";
-const  thankYouPage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/HTML/thanks.html";
-const   HomePage = "https://grupo-novaes.github.io/Treinamento-Seguranca-Trabalho/index.html";
+const  LoginPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/HTML/login.html";
+const   VideoPage = "https://grupo-novaes.github.io/Trainings_Simple_Platformo/HTML/videos.html";
+const   FormsPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/HTML/indexForms.html";
+const  thankYouPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/HTML/thanks.html";
+const   HomePage = "https://grupo-novaes.github.io/Trainings_Simple_Platform/index.html";
 
 //const  LoginPage = "/HTML/login.html";
 //const   VideoPage = "/HTML/videos.html";
